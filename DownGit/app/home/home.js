98f2509b09f9de69d2a7b708f4897bf322900e26
nu11secur1ty <venvaropt@gmail.com>
2025-07-02 13:36:52 +0300

    Update home.js

--- DownGit/app/home/home.js
+++ DownGit/app/home/home.js
@@ -33,7 +33,7 @@ homeModule.config([
 
                     var templateUrl = "https?://github.com/.+/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = "https://minhaskamal.github.io/DownGit/"+downloadUrlInfix;
+                    var downloadUrlPrefix = "https://nu11secur1ty.github.io/DownGit/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
